ae532c7559ba1b37abff200ce4f07cbbd50ab015
23Jam <mafuentes@mail.csuchico.edu>
2023-05-05 11:22:45 -0700

    Changes were made

---
+++ Feature Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody enemyRigidBody;
+
+    public Transform arrowSpawnPoint;
+    public GameObject sheildPrefab;
+
+    public void Arrow()
+    {
+        //Debug.Log(context);
+        {
+
+        }
+    }
+}
--- Feature Project/Assets/Scripts/PlayerController.cs
+++ Feature Project/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rigidBody;
     private PlayerInput playerInputAction;
 
+    public Transform sheildSpawnPoint;
+    public GameObject sheildPrefab;
+
     private void Awake()
     {
         playerInputAction = new PlayerInput();
@@ -34,21 +37,23 @@ public class PlayerController : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         Debug.Log(context);
-        if (context.performed)
+        //if (context.performed)
         {
             Vector2 moveVec = context.ReadValue<Vector2>();
             rigidBody.AddForce(new Vector3(moveVec.x, 0, moveVec.y) * 5, ForceMode.Force);
         }
     }
 
-    /*
+    
     public void Sheild(InputAction.CallbackContext context)
     {
         Debug.Log(context);
-        if (context.performed)
+        //if (context.performed)
         {
-            Debug.Log("The Sheild button is pressed." + context.phase);
+            Debug.Log("The Sheild button is pressed.");
+            var sheild = Instantiate(sheildPrefab, sheildSpawnPoint.position, sheildSpawnPoint.rotation);
+            sheild.GetComponent<Rigidbody>().velocity = sheildSpawnPoint.position;
         }
     }
-    */
+    
 }
